d27d28fdc0dba0d71d972179b9e2ac18a8bbebb6
Walter Cordero <walter.cordero@levvel.io>
2022-01-09 06:39:43 -0600

    :hammer: se agregan rutas

--- server/init/routes.js
+++ server/init/routes.js
@@ -5,5 +5,30 @@ const handlers = require('../handlers')
 
 routerInstance.get('/', handlers.package.version)
 
+/**
+ * Auth
+ */
+routerInstance.post('/registro', handlers.usuarios.store)
+routerInstance.post('/login', handlers.auth.login)
+routerInstance.put('/recovery/:id', handlers.usuarios.recovery)
+/**
+ * Usuarios
+ */
+routerInstance.get('/usuarios', handlers.usuarios.all)
+routerInstance.get('/usuarios/:id', handlers.usuarios.one)
+routerInstance.put('/usuarios/:id', handlers.usuarios.update)
+routerInstance.del('/usuarios/:id', handlers.usuarios.delete)
+/**
+ * Usuarios
+ */
+routerInstance.get('/consultas', handlers.consultas.all)
+routerInstance.get('/consultas/:id', handlers.consultas.one)
+routerInstance.post('/consultas/imagenes', handlers.consultas.pictures)
+/**
+ * Usuarios
+ */
+routerInstance.get('/favoritos/:id', handlers.favoritos.one)
+routerInstance.post('/favoritos', handlers.favoritos.store)
+routerInstance.del('/favoritos/:id', handlers.favoritos.delete)
 
 module.exports = routerInstance
